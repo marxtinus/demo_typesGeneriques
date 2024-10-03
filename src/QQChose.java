010d0687b7f0e077fcb932ec79c7f288c74dd243
martin.bergeron <marxtinus@gmail.com>
2024-10-03 09:10:35 -0400

    corrigé

--- src/QQChose.java
+++ src/QQChose.java
@@ -1,4 +1,4 @@
-public class QQCHose public class QQChose <T> extends PeutEtre<T> {
+public class QQChose <T> extends PeutEtre <T>{
     protected T valeur;
     public QQChose( T a_valeur ){
         valeur = a_valeur;
@@ -12,5 +12,4 @@ public class QQCHose public class QQChose <T> extends PeutEtre<T> {
     public T qQChose() throws ARien{
         return valeur;
     }
-}{
 }
